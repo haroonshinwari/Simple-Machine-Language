ec3ef50ef91a593a8c2179523edfee8a0540c352
Haroon <hshinw01@dcs.bbk.ac.uk>
2019-03-07 17:21:41 +0000

    created a subtraction instruction class

---
+++ src/sml/instructions/SubInstruction.java
@@ -0,0 +1,13 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+// This class represents the subtract instruction - @HaroonShinwari
+
+
+public final class SubInstruction extends Instruction {
+
+
+    
+}
