d91628f748b58c8243678e8973e8b55f5cecb39f
Haroon <hshinw01@dcs.bbk.ac.uk>
2019-03-07 17:23:20 +0000

    added constructors for the subtraction instruction class

--- src/sml/instructions/SubInstruction.java
+++ src/sml/instructions/SubInstruction.java
@@ -7,7 +7,31 @@ import sml.Machine;
 
 
 public final class SubInstruction extends Instruction {
+    private int result;
+    private int op1;
+    private int op2;
+
+    /**
+     * @param label of the instruction
+     * @param op    the operands
+     */
+    public SubInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    /**
+     * @param label     of the instruction
+     * @param res       of the computation
+     * @param operator1 the first operand
+     * @param operator2 the second operand
+     */
+    public SubInstruction(String label, int res, int operator1, int operator2) {
+        this(label, "add");
+        this.result = res;
+        this.op1 = operator1;
+        this.op2 = operator2;
+    }
 
 
-    
+
 }
